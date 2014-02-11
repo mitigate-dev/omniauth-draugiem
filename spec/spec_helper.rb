394b34abd16b9457d2c888bb6979dc1b5c4ac372
Uldis <uldiszz22@gmail.com>
2014-02-11 17:18:00 +0200

    Add spec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'simplecov'
+SimpleCov.start
+require 'rspec'
+require 'rack/test'
+require 'webmock/rspec'
+require 'omniauth-draugiem'
+require 'omniauth'
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+  config.include WebMock::API
+end
